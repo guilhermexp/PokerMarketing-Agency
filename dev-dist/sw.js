fe24c12444a6e3389dbb185961f5bd74a220b4ec
guilhermexp <guilherme-varela@hotmail.com>
2026-01-08 19:01:44 -0300

    Sincronizei o código do index.mjs com o dev-api.mjs.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.r4sdeartqu8"
+    "revision": "0.9p2o4g3s7l4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
