c2e1ccf8b0af2acbdb2119e749b5e821682222c4
guilhermexp <guilherme-varela@hotmail.com>
2026-01-12 19:41:20 -0300

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.a3507i4iv04"
+    "revision": "0.895ael37sh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
